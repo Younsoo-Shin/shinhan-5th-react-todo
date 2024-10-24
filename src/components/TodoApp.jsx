5d3f109423666743e1f43e0bd98225f5bf19c1af
BeTweak <ys@betweak.com>
2024-10-24 16:09:00 +0900

    Create Base TodoList

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,6 +1,29 @@
+import { useCallback, useState } from 'react';
 import './TodoApp.css';
 
+const COLOR_PICK_LIST = ['white', 'red', 'yellow', 'pink', 'green', 'violet'];
+
 export default function TodoApp({}) {
+  const [todoInput, setTodoInput] = useState('');
+  const [todoArray, setTodoArray] = useState([
+    {
+      text: 'initialTodo',
+      color: 'green',
+    },
+  ]);
+  const [pickedColor, setPickedColor] = useState(COLOR_PICK_LIST[0]);
+
+  const addTodo = useCallback(() => {
+    setTodoArray((prevTodoArr) => [
+      ...prevTodoArr,
+      {
+        text: todoInput,
+        color: pickedColor,
+      },
+    ]);
+    setTodoInput('');
+  }, [todoInput, pickedColor]);
+
   return (
     <div className="todoapp-wrap">
       <div className="todoapp">
@@ -9,17 +32,36 @@ export default function TodoApp({}) {
             <h1>Todo APP</h1>
           </div>
           <div style={{ display: 'flex' }}>
-            <input type="text" style={{ flexGrow: 1 }} />
-            <button>입력</button>
+            <input
+              type="text"
+              style={{ flexGrow: 1, backgroundColor: pickedColor }}
+              value={todoInput}
+              onChange={(e) => {
+                setTodoInput(e.target.value);
+              }}
+            />
+            <button onClick={addTodo}>입력</button>
           </div>
         </div>
 
-        <div style={{ marginTop: 30 }}>
-          <div>
-            <button className="colorpick-btn"></button>
-            <button className="colorpick-btn"></button>
-            <button className="colorpick-btn"></button>
-            <button className="colorpick-btn"></button>
+        <div style={{ marginTop: 30, width: '100%' }}>
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'space-evenly',
+              maxWidth: 200,
+              margin: 'auto',
+            }}
+          >
+            {COLOR_PICK_LIST.map((color, idx) => (
+              <button
+                className="colorpick-btn"
+                style={{ backgroundColor: color }}
+                onClick={() => {
+                  setPickedColor(color);
+                }}
+              ></button>
+            ))}
           </div>
         </div>
 
@@ -30,10 +72,13 @@ export default function TodoApp({}) {
 
           <div style={{ width: '100%' }}>
             <ul>
-              <li class="todo-item">item1</li>
-              <li class="todo-item">item2</li>
-              <li class="todo-item">item3</li>
-              <li class="todo-item">item4</li>
+              {todoArray.map((todo) => {
+                return (
+                  <li className="todo-item" style={{ backgroundColor: todo.color }}>
+                    {todo.text}
+                  </li>
+                );
+              })}
             </ul>
           </div>
         </div>
