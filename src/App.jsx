808925cb91608fdc44751f602983aa66d3426dca
BeTweak <ys@betweak.com>
2024-11-01 11:19:03 +0900

    Install bootstrap & Create Component(Login)

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,17 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 import TodoApp from './components/TodoApp';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import TodoContextProvider from './contexts/todoContext';
+
+import Login from './components/Login';
 
 function App() {
   return (
     <>
+      <Login />
+
       <TodoContextProvider>
         <TodoApp />
       </TodoContextProvider>
